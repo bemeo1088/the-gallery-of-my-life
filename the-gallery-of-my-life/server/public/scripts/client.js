59f5a2d52fcc9e3b85ea238c3c0a00ac52c05efc
Hanna Nguyen <ha.nguyenthidan@gmail.com>
2017-10-27 15:45:09 -0500

    Get server up

---
+++ the-gallery-of-my-life/server/public/scripts/client.js
@@ -0,0 +1,15 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('welcomeController', function (){
+    var welcome = this;
+    welcome.record = {
+        name: '',
+        description: ''
+    };
+    welcome.recordList = [];
+
+    
+
+
+
+})
---
+++ the-gallery-of-my-life/server/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var port = 5000;
+
+//Serve up static files
+app.use(express.static('server.public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var recordCollection = [];
+
+//Record POST route
+app.post('/records', function(req, res){
+    console.log(req.body);
+    var recordTosend = req.body;
+    recordCollection.push(recordTosend);
+    res.sendStatus(201);
+    
+});
+
+// Record GET route
+app.get('/records', function(req, res){
+    res.send(recordCollection);
+});
+
+
+
+// Start up the server
+app.listen(port, function(){
+    console.log('listening on port la la la', port);
+    
+});
