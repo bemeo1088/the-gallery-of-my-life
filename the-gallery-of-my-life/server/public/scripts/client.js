1569fdcb9d5cb7af3ffa88401a1cfde394f126f8
Hanna Nguyen <ha.nguyenthidan@gmail.com>
2017-10-29 21:43:01 -0500

    Completed base mode

--- the-gallery-of-my-life/server/public/scripts/client.js
+++ the-gallery-of-my-life/server/public/scripts/client.js
@@ -1,35 +1,20 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('welcomeController', function (){
-    var welcome = this;
- 
+myApp.controller('welcomeController', function ($scope){
     
-
-    var picture = [{
-        name: "My 1st picture",
-        pic: "/images/pic-1.png",
-        likes: 0
-    },
-        name: "My 2nd picture",
-        pic: "/images/pic-2.png",
-        likes: 
-},
-
-
-]
-    
-
-    welcome.likes = function (picture.likes){
-        
+   
+    $scope.hides = [false, false, false, false, false, false];
+    $scope.ShowHide = function (number) {
+        $scope.hides[number] = !$scope.hides[number];
     }
-    // welcome.addRecord = function(){
-    //     var record = {
-    //         description: welcome.description,
-    //         url: welcome.url
-    //     }
-
-    // }
-
-
-
-})
+    $scope.like = function (picture) {
+        picture.like +=1;
+    }
+    $scope.pictures = [{ name: "Our first trip to Minehaha Falls", pic: "pic-1.png", like: 0, index:0},
+                    {name: "Autumn in Richmond, VA", pic: "pic-2.png", like: 0, index:1},
+                    {name: "Autumn in Richmond, VA", pic: "pic-3.png", like: 0, index:2},
+                    {name: "Just a beautiful day at Minnesota State Park", pic: "pic-4.png", like: 0, index:3},
+                    {name: "The beautiful Hanging Lake in Colorado", pic: "pic-5.png", like: 0, index:4},
+                    {name: "Hiking in Aspen, CO to celebrate our wedding anniversary", pic: "pic-6.png", like: 0, index:5}];
+   
+});
